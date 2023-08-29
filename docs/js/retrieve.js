5fb885fe85f53ce2790e53643336b6e9783e6a65
Daily News <hello@thedailynews.ink>
2023-08-29 14:29:39 +0500

    V2.08.alpha.14
    -> Changes

--- docs/js/retrieve.js
+++ docs/js/retrieve.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js';
-import { getAuth, getRedirectResult, GoogleAuthProvider, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js';
+import { getAuth, getRedirectResult, GoogleAuthProvider, onAuthStateChanged, signOut, signInWithRedirect } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDtrP8PX_1n_q0qQvMvs_llbpfZ03IjyV0",
@@ -12,55 +12,57 @@ const firebaseConfig = {
   measurementId: "G-NXVSPGLJZC"
 };
 
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+
 getRedirectResult(auth)
   .then((result) => {
     function updateLoginButton(user) {
-        const loginOrOutButton = document.getElementById('loginorout');
-        const userMenuButton = document.getElementById('user-menu-button');
-        const userProfilePicture = document.getElementById('imgPfp');
-      
-        if (user) {
-          // User is logged in
-          loginOrOutButton.textContent = "Log out ←⠀";
-          userMenuButton.style.display = "block"; // Show user menu button
-          userProfilePicture.src = user.photoURL; // Set user profile picture
-      
-          loginOrOutButton.addEventListener('click', () => {
-            signOut(auth)
-              .then(() => {
-                console.log('User signed out successfully.');
-              })
-              .catch((error) => {
-                console.error('Error signing out:', error);
-              });
-          });
-        } else {
-          // User is not logged in
-          loginOrOutButton.textContent = "Log in →";
-          userMenuButton.style.display = "none"; // Hide user menu button
-      
-          loginOrOutButton.addEventListener('click', () => {
-            // Redirect the user to the login page
-            signInWithRedirect(auth, provider);
-          });
-        }
+      const loginOrOutButton = document.getElementById('loginorout');
+      const userMenuButton = document.getElementById('user-menu-button');
+      const userProfilePicture = document.getElementById('imgPfp');
+
+      if (user) {
+        // User is logged in
+        loginOrOutButton.textContent = "Log out ←⠀";
+        userMenuButton.style.display = "block"; // Show user menu button
+        userProfilePicture.src = user.photoURL; // Set user profile picture
+
+        loginOrOutButton.addEventListener('click', () => {
+          signOut(auth)
+            .then(() => {
+              console.log('User signed out successfully.');
+            })
+            .catch((error) => {
+              console.error('Error signing out:', error);
+            });
+        });
+      } else {
+        // User is not logged in
+        loginOrOutButton.textContent = "Log in →";
+        userMenuButton.style.display = "none"; // Hide user menu button
+
+        loginOrOutButton.addEventListener('click', () => {
+          // Redirect the user to the login page
+          signInWithRedirect(auth, provider);
+        });
       }
-      
-      // Update the login/logout button and user profile picture based on the authentication state
-      onAuthStateChanged(auth, (user) => {
-          updateLoginButton(user);
-          const userMenuButton = document.getElementById('user-menu-button');
-          const userProfilePicture = document.getElementById('imgPfp');
-          
-          if (user) {
-              userMenuButton.style.display = 'block';
-              userProfilePicture.src = user.photoURL;
-          } else {
-              userMenuButton.style.display = 'none';
-              userProfilePicture.src = ''; // Clear the source to hide the image
-          }
-      });
+    }
+
+    // Update the login/logout button and user profile picture based on the authentication state
+    onAuthStateChanged(auth, (user) => {
+      updateLoginButton(user);
+      const userMenuButton = document.getElementById('user-menu-button');
+      const userProfilePicture = document.getElementById('imgPfp');
+
+      if (user) {
+        userMenuButton.style.display = 'block';
+        userProfilePicture.src = user.photoURL;
+      } else {
+        userMenuButton.style.display = 'none';
+        userProfilePicture.src = ''; // Clear the source to hide the image
+      }
     });
+  });
