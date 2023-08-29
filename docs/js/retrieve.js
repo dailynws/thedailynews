d1f6e5a23c59ec913cef8f19f346479564686a78
Daily News <hello@thedailynews.ink>
2023-08-29 14:07:17 +0500

    V2.08.alpha.12
    -> Minor Changes

--- docs/js/retrieve.js
+++ docs/js/retrieve.js
@@ -14,6 +14,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const provider = new GoogleAuthProvider();
 getRedirectResult(auth)
   .then((result) => {
     function updateLoginButton(user) {
